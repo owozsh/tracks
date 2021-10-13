b9da88fec821fae299f8fd3c65e31ea921e54ff6
owozsh <owozsh@posteo.net>
2021-10-13 09:40:28 -0300

    initial setup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SigninScreen from './src/screens/SigninScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tracks" component={SigninScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/SignupScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+  return <Text>SignupScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
